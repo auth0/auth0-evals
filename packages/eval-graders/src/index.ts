85ccc773b7066b0772e27bada901764a0aada05a
Frederik Prijck <frederik.prijck@okta.com>
2026-06-16 15:16:27 +0200

    feat(graders): add compiles() grader primitive

--- packages/eval-graders/src/index.ts
+++ packages/eval-graders/src/index.ts
@@ -2,7 +2,7 @@
 
 // Types
 export { GraderLevel } from './types.js';
-export type { GraderResult, GraderDef, GraderOptions, EventToolCall, EventGraderLevel } from './types.js';
+export type { GraderResult, GraderDef, GraderOptions, EventToolCall, EventGraderLevel, CompileResult } from './types.js';
 
 // Grader factory functions
 export {
@@ -14,4 +14,5 @@ export {
   ranCommand,
   ranCommandOneOf,
   wroteFile,
+  compiles,
 } from './primitives.js';
